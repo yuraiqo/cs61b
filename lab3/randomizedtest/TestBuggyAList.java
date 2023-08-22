d7aec7ce9f262d8258088ae37fd45085cb511ff0
Juraj Siroky <siroky.juraj0@gmail.com>
2023-08-23 01:20:17 +0200

    Finished lab3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -34,7 +34,7 @@ public class TestBuggyAList {
         AListNoResizing<Integer> L = new AListNoResizing<>();
         BuggyAList<Integer> L2 = new BuggyAList<>();
 
-        int N = 500;
+        int N = 5000;
         for (int i = 0; i < N; i += 1) {
             int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {
@@ -42,24 +42,20 @@ public class TestBuggyAList {
                 int randVal = StdRandom.uniform(0, 100);
                 L.addLast(randVal);
                 L2.addLast(randVal);
-                System.out.println("addLast(" + randVal + ")");
             } else if (operationNumber == 1) {
                 // size
                 int size = L.size();
                 int size2 = L2.size();
-                System.out.println("size: " + size + " size2: " + size2);
                 assertEquals(size, size2);
             } else if (operationNumber == 2 && L.size() > 0 && L2.size() > 0) {
                 // getLast
                 int last = L.getLast();
                 int last2 = L2.getLast();
-                System.out.println("getLast: " + last + " getLast2: " + last2);
                 assertEquals(last, last2);
             } else if (operationNumber == 3 && L.size() > 0 && L2.size() > 0) {
                 // removeLast
                 int removed = L.removeLast();
                 int removed2 = L2.removeLast();
-                System.out.println("removeLast: " + removed + " removeLast2: " + removed2);
                 assertEquals(removed, removed2);
             }
         }
