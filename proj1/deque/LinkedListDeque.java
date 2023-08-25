430ebe2a2017aaebdeddf4d30778328f18be87aa
Juraj Siroky <siroky.juraj0@gmail.com>
2023-08-25 23:45:51 +0200

    Updated proj1

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,29 @@
+package deque;
+
+public class LinkedListDeque<T> {
+    public void addFirst(T item) {};
+
+    public void addLast(T item) {};
+
+    public boolean isEmpty() {
+        return false;
+    };
+
+    public int size() {
+        return 0;
+    };
+
+    public void printDeque() {};
+
+    public T removeFirst() {
+        return null;
+    };
+
+    public T removeLast() {
+        return null;
+    };
+
+    public T get(int index) {
+        return null;
+    };
+}
