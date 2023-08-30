4d4afcd9cd504a00ce61d1a7e2b9f0439ff1e69a
Juraj Siroky <siroky.juraj0@gmail.com>
2023-08-30 22:11:07 +0200

    Updated ArrayDeque

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,15 +1,16 @@
 package deque;
 
 public class ArrayDeque<T> {
+
     private T[] items = (T[]) new Object[8];
     private int size;
     private int first;
     private int last;
 
+    /** Creates empty ArrayDeque. */
     public ArrayDeque() {
         size = 0;
-        first = 0;
-        last = items.length - 1;
+        first = last = -1;
     }
 
     public void resize() {
@@ -18,34 +19,91 @@ public class ArrayDeque<T> {
         }
     }
 
+    /** Adds item to start of ArrayDeque. */
     public void addFirst(T item) {
-        size += 1;
         resize();
-        first = (first == 0) ? size : first - 1;
+        if (first == -1 && last == -1) {
+            first = last = 0;
+            items[first] = item;
+        } else if (first == 0) {
+            first = items.length - 1;
+            items[first] = item;
+        } else {
+            first -= 1;
+            items[first] = item;
+        }
+
     }
 
+    /** Adds item to end of ArrayDeque. */
     public void addLast(T item) {
-        size += 1;
         resize();
-        last = (last + 1 == size) ? 0 : last + 1;
-        items[last] = item;
+        if (first == -1 && last == -1) {
+            first = last = 0;
+            items[last] = item;
+        } else if (last == items.length - 1) {
+            last = 0;
+            items[last] = item;
+        } else {
+            last += 1;
+            items[last] = item;
+        }
     }
 
+    /** Returns true if ArrayDeque is empty, otherwise returns false. */
     public boolean isEmpty() {
         return size <= 0;
     }
 
+    /** Returns size of ArrayDeque. */
     public int size() {
         return size;
     }
 
+    /** Prints formatted ArrayDeque. */
     public void printDeque() {
-        return;
+        int i = first;
+        while (i != last) {
+            System.out.print(items[i] + " ");
+            i = (i + 1) % items.length;
+        }
+        System.out.print(items[last]);
+        System.out.println();
     }
 
-    public T removeFirst() {}
+    /** Removes first item in ArrayDeque. */
+    public T removeFirst() {
+        T item = items[first];
+        items[first] = null;
+        first += 1;
+        size -= 1;
+        return item;
+    }
 
-    public T removeLast() {}
+    /** Removes last item in ArrayDeque. */
+    public T removeLast() {
+        T item = items[last];
+        items[last] = null;
+        last -= 1;
+        size -= 1;
+        return item;
+    }
 
-    public T get(int index) {}
+    /** Returns nth item in ArrayDeque. */
+    public T get(int index) {
+        if (index < 0 || index >= items.length) {
+            return null;
+        }
+        return items[first + index];
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> list = new ArrayDeque<>();
+        list.addFirst(5);
+        list.addFirst(1);
+        list.addFirst(3);
+        list.addLast(9);
+        list.addFirst(10);
+        list.printDeque();
+    }
 }
